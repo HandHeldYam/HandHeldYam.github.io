e5b38b86bba38fbe438843e0ec68063c7f061588
Sammy Collins <sammy.collins47@gmail.com>
2020-06-19 10:11:03 -0400

    feat: add API and backend functionality

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const app = express();
 
 app.listen(3000, () => console.log('Listening on Port 3000'));
 app.use(express.static('public'));//uses files in the "public" folder
+app.use(express.json({ limit: '1mb' }));
+ 
+app.post('/api', (request, response) => {
+    console.log("request recieved");
+    console.log(request.body);
+});
