fe77089be313c54c4185df70627b8c400f15f294
Sammy Collins <sammy.collins47@gmail.com>
2020-06-22 10:17:30 -0400

    feat: add socket.io

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const dirName = 'HandHeldYam.github.io/';
 
-app.listen(3000, () => console.log('Listening on Port 3000'));
+http.listen(3000, () => console.log('Listening on Port 3000'));
 app.use(express.static('public'));//uses files in the "public" folder
 app.use(express.json({ limit: '1mb' }));
 var participants = [];
+app.get('/', (req, res) => {
+    res.sendFile(dirName + '/index.html');
+    console.log("used me");
+});
 app.post('/UserApi', (request, response) => {
     console.log("request recieved");
     console.log(request.body);
