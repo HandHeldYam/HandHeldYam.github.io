89598c9a99b6e29fb6965df618d47c99c1b94aca
Sammy Collins <sammy.collins47@gmail.com>
2020-06-23 11:01:17 -0400

    feat: test commit

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const server = require('http').Server(app);
+const io = require('socket.io').listen(server);
 const dirName = 'HandHeldYam.github.io/';
 
-http.listen(3000, () => console.log('Listening on Port 3000'));
+server.listen(3000, () => console.log('Listening on Port 3000'));
 app.use(express.static('public'));//uses files in the "public" folder
 app.use(express.json({ limit: '1mb' }));
 var participants = [];
-app.get('/', (req, res) => {
-    res.sendFile(dirName + '/index.html');
-    console.log("used me");
-});
-// app.get('/connectionTest', (req, res) => {
-//     res.sendFile('connectionTest.html');
-//     console.log('sending connectionTest page');
-// })
+
 io.on('connection', (socket) => {
     console.log('a user connected');
-})
+});
+
 app.post('/UserApi', (request, response) => {
     console.log("request recieved");
     console.log(request.body);
