66448108209455842e9115e3d649a9640d922dfd
Sammy Collins <sammy.collins47@gmail.com>
2020-06-26 13:36:09 -0400

    fix: merge issues

--- index.js
+++ index.js
@@ -31,28 +31,46 @@ var io = socket(server);
 
 const dirName = 'HandHeldYam.github.io/';
 server.listen(3000, () => console.log('Listening on Port 3000'));
-app.use(express.static('public'));//uses files in the "public" folder
+app.use(express.static('public'));//listens to files in the "public" folder
 app.use(express.json({ limit: '1mb' }));
 var clients = [];
+app.get('/', (req, res) => {
+    res.send('mainMenu.html', { root: "public/mainMenu.html" })
+});
+app.post('/UserApi', (request, response) => {
+    console.log("request recieved");
+    console.log(request.body);
+    console.log("USer added to list");//no users actually added
+});
+app.post('/api', (request, response) => {
+    console.log("request recieved");
+    console.log(request.body);
 
-// app.get('/', (req, res) => {
-//     res.sendFile(dirName + '/connectionTest.html');
-// });
+});
+app.post('/roomCodeApi', (request, response) => {
+    console.log("request recieved");
+    console.log(request.body);
+});
+
 io.sockets.on('connection', onConnect);
 function onConnect(socket) {
     console.log('new connection' + socket.id);
     c = new Client(socket.id, "temp", "get from json");
     clients.push(Client);
     console.log(clients);
+    
 }
-io.sockets.on("newClient", function handleClient() {
+io.sockets.on("newClient", function handleClient(data) {
     console.log("new Client connected");
 });
 
-function Client(id, name, type) {
+class Client{
+    constructor(id, name, type) {
     this.id = id;
     this.name = name;
     this.type = type;
+    }
+
 }
 
 app.post('/UserApi', (request, response) => {
@@ -71,8 +89,4 @@ app.post('/api', (request, response) => {
     console.log("request recieved");
     console.log(request.body);
 
-});
-app.post('/roomCodeApi', (request, response) => {
-    console.log("request recieved");
-    console.log(request.body);
 });
