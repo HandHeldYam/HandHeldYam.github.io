ada09af76279ddbda6b3eca3169ec6400f28b406
Fitz <nicole.fitz@caci.com>
2020-07-29 16:39:38 -0400

    feat: game options

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-app.use(express.static('public'));//listens to files in the
+app.use(express.static('public')); //listens to files in the
 let numClients = -1; //sm doesnt count as player but i have no idea if this logic is right lol
 //"public" folder
 //app.use('public', express.static(__dirname));
@@ -12,7 +12,7 @@ var io = require('socket.io')(server);
 var validRoomCodes = new Map();
 var issues = [];
 //creates array for card values
-var cardNumbers = Array(9);//9 card values in planning poker
+var cardNumbers = Array(9); //9 card values in planning poker
 
 server.listen(8080, () => console.log('Listening on Port 8080'));
 
@@ -20,34 +20,33 @@ server.listen(8080, () => console.log('Listening on Port 8080'));
 
 app.get('/', (req, res) => {
     console.log('hellllo');
-   res.sendFile('index.html', { root:'./' });
-   console.log('sent to main menu');
+    res.sendFile('index.html', { root: './' });
+    console.log('sent to main menu');
 });
 
 app.get('/index', (req, res) => {
-    res.sendFile('index.html', {root: './'});
+    res.sendFile('index.html', { root: './' });
 })
 
 app.get('/login', (req, res) => {
-   res.sendFile('login.html',{root: './'});
+    res.sendFile('login.html', { root: './' });
 });
 
 app.get('/lobby', (req, res) => {
-    res.sendFile('lobby.html', {root: './'});
+    res.sendFile('lobby.html', { root: './' });
 });
 
 app.get('/planningPokerScreen', (req, res) => {
-    res.sendFile('planningPokerScreen.html', {root: './'});
-    }
-);
+    res.sendFile('planningPokerScreen.html', { root: './' });
+});
 
 app.get('/underConstruction', (req, res) => {
     res.sendFile('underConstruction.html', { root: './' });
 });
 app.post('/roomCodeApi', (req, res) => {
 
-    if (onCorrectRoomCode(req.body.code)) {//if the room code is correct
-        res.sendStatus(201);//send a 201 rather than 200
+    if (onCorrectRoomCode(req.body.code)) { //if the room code is correct
+        res.sendStatus(201); //send a 201 rather than 200
 
     }
 });
@@ -56,45 +55,45 @@ app.post('/roomCodeApi', (req, res) => {
 
 
 
-io.sockets.on('connection', onConnect);//io.sockets = default namespace (/)
+io.sockets.on('connection', onConnect); //io.sockets = default namespace (/)
 
-io.sockets.use((socket, next) => {//idk what this does
+io.sockets.use((socket, next) => { //idk what this does
 
-  next();
+    next();
 });
 
-	//when the server receives clicked message, do this
-    // client.on('clicked', function(data) {
-    // 	  cardNumbers=[1,2,3,5,8,13,21,"unsure", "infinity"];
+//when the server receives clicked message, do this
+// client.on('clicked', function(data) {
+// 	  cardNumbers=[1,2,3,5,8,13,21,"unsure", "infinity"];
 
-    //     if(socket.vote==1){
-    //        io.emit('clicked',cardNumbers[0]);
-    //     }
-    //     if(socket.vote==2){
-    //       io.emit('clicked',cardNumbers[1]);
-    //     }
-    //     if(socket.vote==3){
-    //       io.emit('clicked',cardNumbers[2]);
-    //     }
-    //     if(socket.vote==5){
-    //       io.emit('clicked',cardNumbers[3]);
-    //     }
-    //     if(socket.vote==8){
-    //       io.emit('clicked',cardNumbers[4]);
-    //     }
-    //     if(socket.vote==13){
-    //       io.emit('clicked', cardNumbers[5]);
-    //     }
-    //     if(socket.vote==21){
-    //       io.emit('clicked', cardNumbers[6]);
-    //     }
-    //     if(socket.vote==0){
-    //       io.emit('clicked', cardNumbers[7]);
-    //     }
-    //     if(socket.vote==100){
-    //       io.emit('clicked', cardNumbers[8]);
-    //     }
-    // });
+//     if(socket.vote==1){
+//        io.emit('clicked',cardNumbers[0]);
+//     }
+//     if(socket.vote==2){
+//       io.emit('clicked',cardNumbers[1]);
+//     }
+//     if(socket.vote==3){
+//       io.emit('clicked',cardNumbers[2]);
+//     }
+//     if(socket.vote==5){
+//       io.emit('clicked',cardNumbers[3]);
+//     }
+//     if(socket.vote==8){
+//       io.emit('clicked',cardNumbers[4]);
+//     }
+//     if(socket.vote==13){
+//       io.emit('clicked', cardNumbers[5]);
+//     }
+//     if(socket.vote==21){
+//       io.emit('clicked', cardNumbers[6]);
+//     }
+//     if(socket.vote==0){
+//       io.emit('clicked', cardNumbers[7]);
+//     }
+//     if(socket.vote==100){
+//       io.emit('clicked', cardNumbers[8]);
+//     }
+// });
 
 function onConnect(socket) {
     socket.emit('hello', 'hello');
@@ -116,19 +115,19 @@ function onConnect(socket) {
     socket.on('start', (data) => {
         if (data.type === 'Scrum Master') {
             io.in(data.code).emit('start');
-            
+
         }
     });
     socket.on('reconnection', () => {
         socket.emit('giveData');
     });
-    socket.on('imBack', (data) => {//client gives name, type, and code
-        
+    socket.on('imBack', (data) => { //client gives name, type, and code
+
     });
     socket.on("disconnect", () => {
         let roomToBUpdated;
         for (const [key, value] of validRoomCodes.entries()) {
-            for (var i = 0; i < value.users.length; i++){
+            for (var i = 0; i < value.users.length; i++) {
                 if (value.users[i].id === socket.id) {
                     roomToBUpdated = key;
                     value.users.splice(i, 1);
@@ -145,11 +144,11 @@ function handleClient(data, socket) {
     console.log('handling client ' + socket.id);
 
     if (onCorrectRoomCode(data.code)) {
-        socket.join(data.code);//this is what joins socket to room: data.code
-        validRoomCodes.get(data.code).users.push({ name: data.name, type: data.type, id: socket.id });//add the socket to the list of users
+        socket.join(data.code); //this is what joins socket to room: data.code
+        validRoomCodes.get(data.code).users.push({ name: data.name, type: data.type, id: socket.id }); //add the socket to the list of users
         let users = validRoomCodes.get(data.code).users; // get the list of users 
-        io.in(data.code).emit('displayName', { users: users, code: data.code });//tell users to display
-        
+        io.in(data.code).emit('displayName', { users: users, code: data.code }); //tell users to display
+
     }
 }
 
@@ -160,7 +159,7 @@ function onCorrectRoomCode(code) {
 
 function addRoom(socket, data) {
 
-    if (!validRoomCodes.has(data.code)) {//if there are no rooms with code: data.code
+    if (!validRoomCodes.has(data.code)) { //if there are no rooms with code: data.code
         validRoomCodes.set(data.code, { users: [], name: data.name });
         socket.emit('joinRoom', data.code);
     }
